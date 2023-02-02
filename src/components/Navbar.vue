6195f2d838063442e2b719b0a7ee1e7826912489
6-ON <aminhatim6@gmail.com>
2023-02-02 22:49:03 +0100

    add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+<nav class="flex gap-3 justify-center">
+  <router-link to="/login">login</router-link>
+  <router-link to="/register">register</router-link>
+  <router-link to="/">home</router-link>
+  <router-link to="/reserve">reserve</router-link>
+
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
