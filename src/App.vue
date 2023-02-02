5d94bc2c0ff801de362a51e85b710adaa7df6a3b
6-ON <aminhatim6@gmail.com>
2023-02-02 22:49:14 +0100

    test router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+
+import Navbar from "@/components/Navbar.vue";
+import {ref} from "vue";
+
+const isAdmin = ref()
 </script>
 
 <template>
+  <Navbar></Navbar>
+<router-view></router-view>
 </template>
 
