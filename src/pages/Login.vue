256860e7967802b500ff01fce59c42519591aab9
6-ON <aminhatim6@gmail.com>
2023-02-05 23:21:22 +0100

    implement login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -6,11 +6,11 @@
         <img src="/logo.png" class="w-16 mb-4" alt="logo">
 
       </router-link>
-      <form class="flex flex-col items-center w-full">
-        <textarea class="form-field" type="text" placeholder="Token"></textarea>
+      <form class="flex flex-col items-center w-full" @submit.prevent="handleLogin">
+        <textarea class="form-field" type="text" placeholder="Token" v-model="token"></textarea>
         <button type="submit" class="form-submit">Login</button>
       </form>
-      <router-link to="/register" class="link" >You don't have a token ?</router-link>
+      <router-link to="/register" class="link">You don't have a token ?</router-link>
     </div>
   </main>
 
@@ -18,9 +18,49 @@
 
 <script>
 import axios from 'axios'
+import Swal from "sweetalert2";
 
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      token: ''
+    }
+  },
+  methods: {
+    handleLogin() {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+        axios.defaults.headers.common['Authorization'] =
+            `Bearer ${this.token}`;
+        axios.post('http://localhost:8080/users/login')
+            .then(res => {
+              const data = res.data
+              Toast.fire({
+                icon: data.type,
+                title: data.content.message
+              })
+              this.$router.push('/')
+
+            })
+            .catch(err => {
+              const data = err.response.data
+              Toast.fire({
+                icon: data.type,
+                title: data.content.message
+              })
+            })
+    }
+  }
 }
 </script>
 
@@ -37,10 +77,12 @@ export default {
 .form-field {
   @apply bg-red-500 h-32 rounded-2xl w-full p-1 text-red-200 outline-none text-center placeholder-red-50 placeholder-opacity-30 resize-none
 }
-.form-submit{
+
+.form-submit {
   @apply text-red-200 bg-red-500 mt-5 rounded-2xl py-2 px-4 w-fit hover:bg-red-50 hover:text-red-500
 }
-.link{
+
+.link {
   @apply text-red-50 hover:underline mt-2
 }
 </style>
