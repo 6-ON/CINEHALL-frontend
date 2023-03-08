47ae1edde5ff3eb8938f76af52abec30e471bbb4
6-ON <aminhatim6@gmail.com>
2023-03-08 22:23:00 +0100

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,65 +1,88 @@
 <script>
 import Sidebar from "@/components/Sidebar.vue";
 import sidebar from "@/components/Sidebar.vue";
-
+import axios from "axios";
 export default {
-  name: "Navbar",
-  components: {Sidebar},
-  data() {
-    return {
-      sidebarShown: false,
-    }
-  },
-  computed:{
+	name: "Navbar",
+	components: { Sidebar },
+	data() {
+		return {
+			sidebarShown: false,
+			user: null
+		}
+	},
+	methods: {
+		toggleMenu() {
+			this.sidebarShown = !this.sidebarShown
+		},
+		async handleLogout(){
+			localStorage.clear();
+			this.user = null
+			this.$router.push('/')
+			await axios.post('http://localhost:8080/logout')
+		}
+	}
+	, mounted() {
+		const storageToken = localStorage.getItem('token');
+		if (storageToken ?? false) {
 
-  },
-  methods: {
-    toggleMenu() {
-      this.sidebarShown = !this.sidebarShown
-    }
-  }
+			axios.defaults.headers.common['Authorization'] = `Bearer ${storageToken}`;
+			axios.get('http://localhost:8080/users/get-info').then((res) => {
+				this.user = res.data;
+			}).catch((er) => this.user = null);
+		}
+
+	}
 }
 </script>
 
 
 <template>
+	<header :class="$route.meta.navbarFixed" class=" w-full z-10 font-sans">
+		<nav
+			class="relative px-4 py-4 flex justify-between items-center bg-white bg-opacity-60 backdrop-blur rounded-b-3xl">
+			<router-link to="/" class="text-3xl font-bold leading-none">
+				<img src="/logo.png" alt="logo" class="w-6">
+			</router-link>
+			<div class="lg:hidden">
+				<button @click="toggleMenu" class="flex items-center text-red-600 p-3">
+					<svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+						<title>Mobile menu</title>
+						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+					</svg>
+				</button>
+			</div>
+			<ul
+				class="text-gray-700 font-semibold hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+				<li>
+					<router-link exact-active-class="text-red-600 font-bold" to="/" class="text-sm">Home</router-link>
+				</li>
+				<li>
+					<router-link exact-active-class="text-red-600 font-bold" to="/films" class="text-sm">Films</router-link>
+				</li>
+				<li v-if="user">
+					<router-link exact-active-class="text-red-600 font-bold" to="/reservations" class="text-sm">Reservations</router-link>
+				</li>
 
-  <header :class="$route.meta.navbarFixed" class=" w-full z-10 font-sans">
-    <nav class="relative px-4 py-4 flex justify-between items-center bg-white bg-opacity-60 backdrop-blur rounded-b-3xl">
-      <router-link to="/" class="text-3xl font-bold leading-none">
-        <img src="/logo.png" alt="logo" class="w-6">
-      </router-link>
-      <div class="lg:hidden">
-        <button @click="toggleMenu" class="flex items-center text-red-600 p-3">
-          <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <title>Mobile menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-          </svg>
-        </button>
-      </div>
-      <ul class="text-gray-700 font-semibold hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-        <li>
-          <router-link exact-active-class="text-red-600 font-bold" to="/" class="text-sm">Home</router-link>
-        </li>
-        <li>
-          <router-link exact-active-class="text-red-600 font-bold" to="/films" class="text-sm">Films</router-link>
-        </li>
-
-      </ul>
-      <router-link to="/login"
-                   class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200">
-        Sign In
-      </router-link>
-      <router-link to="/register"
-                   class="hidden lg:inline-block py-2 px-6 bg-red-500 hover:bg-red-600 text-sm text-white font-bold rounded-xl transition duration-200">
-        Sign up
-      </router-link>
-    </nav>
-  </header>
-  <!--  sidebar-->
-  <sidebar v-if="sidebarShown" @sidebar-close="toggleMenu"></sidebar>
+			</ul>
+			<div v-if="!user">
+				<router-link to="/login"
+					class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200">
+					Sign In
+				</router-link>
+				<router-link to="/register"
+					class="hidden lg:inline-block py-2 px-6 bg-red-500 hover:bg-red-600 text-sm text-white font-bold rounded-xl transition duration-200">
+					Sign up
+				</router-link>
+			</div>
+			<button v-else
+				@click="handleLogout"
+				class="hidden lg:inline-block py-2 px-6 bg-red-500 hover:bg-red-600 text-sm text-white font-bold rounded-xl transition duration-200">logout</button>
+		</nav>
+	</header>
+	<!--  sidebar-->
+	<sidebar v-if="sidebarShown" @sidebar-close="toggleMenu"></sidebar>
 </template>
 
 
-<style scoped>
-</style>
+<style scoped></style>
